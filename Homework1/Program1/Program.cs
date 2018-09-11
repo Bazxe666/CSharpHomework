dcd3a630d27c629de998a17d80856b513255b285
Bazxe666 <641819347@qq.com>
2018-09-11 23:59:25 +0800

    Homework1

---
+++ Homework1/Program1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = "";
+            string num2 = "";
+            int b = 0;
+            int n = 0;
+            Console.Write("请输入第一个数字：");
+            num1 = Console.ReadLine();
+            n = Int32.Parse(num1);
+            Console.Write("请输入第二个数字：");
+            num2 = Console.ReadLine();
+            b = Int32.Parse(num2);
+            Console.Write("结果:" + n * b);
+        }
+    }
+}
---
+++ Homework1/Program2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = Int32.Parse(textBox1.Text);
+            int y = Int32.Parse(textBox2.Text);
+            int z = x * y;
+            string a = z.ToString();
+            label1.Text = a;
+        }
+    }
+}
